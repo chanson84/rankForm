8a348e8fbe811386d73a0dcebcfae78f2172d7dc
Chris <chansonart84@gmail.com>
2020-11-28 15:37:01 -0600

    Initial commit

---
+++ rankForm.js
@@ -0,0 +1,10 @@
+$(document).ready(
+    function () {
+
+//add event handlers
+
+        //all other functions (program logic)
+        //change color for ranks
+var selectedRadioButton = $("input[name=].checked");
+    }
+)
